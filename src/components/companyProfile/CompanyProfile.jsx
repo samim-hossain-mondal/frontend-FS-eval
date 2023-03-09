2c7ce7dc0653d8ad05210cf188b2cab887892962
samim-hossain-mondal <Samim_Hossain_Mondal@mckinsey.com>
2023-03-09 18:33:20 +0530

    feat: make dashboard UI

---
+++ src/components/companyProfile/CompanyProfile.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './CompanyProfile.css';
+
+export default function CompanyProfile() {
+  return (
+    <div className="company-profile">
+      <div className='company-profile-header'>
+        <h2>Content Types</h2>
+      </div>
+      <div>
+        <h3>Content</h3>
+      </div>
+    </div>
+  );
+}
--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,29 +1,28 @@
 import React from 'react';
+import CompanyProfile from '../companyProfile/CompanyProfile';
 import './Dashboard.css';
 
 export default function Dashboard() {
   return (
     <div className="dashboard-page">
       <div className="container-left">
-        <div>
+        <div className='container-left-header'>
           <h1>CMS+</h1>
         </div>
-        <div>
-          <h3>Collection Types</h3>
+        <div className='container-left-content'>
+          <h3>COLLECTION TYPES</h3>
           <ul>
             <li>Blog</li>
             <li>Product</li>
             <li>Page</li>
           </ul>
         </div>
+        <div className='content-type-builder'>
+          <h3>CONTENT TYPE BUILDER</h3>
+        </div>
       </div>
       <div className="container-right">
-        <div>
-          <h3>Content Types</h3>
-        </div>
-        <div>
-          <h3>Content</h3>
-        </div>
+        <CompanyProfile />
       </div>
     </div>
   );
