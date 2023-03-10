56a84dd6f6a235115966262ccc68877ddc32ca15
samim-hossain-mondal <Samim_Hossain_Mondal@mckinsey.com>
2023-03-10 11:38:30 +0530

    feat: delete a single field from collections of datas

--- src/components/entryDetails/EntryDetails.jsx
+++ src/components/entryDetails/EntryDetails.jsx
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
+import ModalForm from '../modalForm/ModalForm';
 import proptypes from 'prop-types';
 import { API_URL } from '../../constants/url';
 import trashimg from '../../assets/trash-delete-recycle-bin-bucket-waste.png';
@@ -7,7 +8,7 @@ import editimg from '../../assets/user-edit-text-message-note.png';
 import './EntryDetails.css';
 
 export default function EntryDetails({collection}) {
-  // console.log(collection);
+  const [show, setShow] = useState(false);
   const [entry, setEntry] = useState([]);
   const fetchEntry= async () => {
     const response = await axios.get(`${API_URL}/collections/${collection.id}`, {
@@ -20,6 +21,31 @@ export default function EntryDetails({collection}) {
   useEffect(() => {
     fetchEntry();
   }, [collection.id]);
+  const onDelete = async (id) => {
+    await axios.delete(`${API_URL}/collections/${id}`, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('token')}`
+      }
+    });
+    fetchEntry();
+  };
+  const handleAddNewEntrySubmit = async (e) => {
+    e.preventDefault();
+    const data = {} ;
+    const formData = new FormData(e.target);
+    for (let [key, value] of formData.entries()) {
+      data[key] = value;
+    }
+    await axios.post(`${API_URL}/collections/${collection.id}`, {
+      entry: data
+    }, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('token')}`
+      }
+    });
+    setShow(false);
+    fetchEntry();
+  };
   return (
     <div className="entry">
       <div className="entry-header">
@@ -31,7 +57,25 @@ export default function EntryDetails({collection}) {
             <h3>{entry.length} {entry.length > 1 ? 'Entries Found' : 'Entry Found'}</h3>
           </div>
           <div className="entry-body-header-2">
-            <button>Add a new entry</button>
+            <button onClick={() => setShow(true)}>Add a new entry</button>
+            <ModalForm title="My Modal" onClose={() => setShow(false)} show={show}
+            >
+              <div className='create-content'>
+                {
+                  <form onSubmit={handleAddNewEntrySubmit}>
+                    {
+                      collection.field.map((item) => (
+                        <div key={item.id}>
+                          <label>{item}</label>
+                          <input type="text" name={item} />
+                        </div>
+                      ))
+                    }
+                    <button id="add-entry-btn">Add</button>
+                  </form>
+                }
+              </div>
+            </ModalForm>
           </div>
         </div>
         <div className="entry-body-content">
@@ -69,7 +113,7 @@ export default function EntryDetails({collection}) {
                           ))
                         }
                         <div className='editors'>
-                          <img src={trashimg} alt="trash" />
+                          <img onClick={() => onDelete(item.id)} src={trashimg} alt="trash" />
                           <img src={editimg} alt="edit" />
                         </div>
                       </div>
